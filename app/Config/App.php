8a75f2964e7f891a65fc52e947c215bd14c7d10e
Jomil Mutya <grizzfrei27@gmail.com>
2024-07-12 22:05:52 +0800

    Changed base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/group1-ecwebsite';
+    public string $baseURL = 'http://localhost/Group1_E-commerce';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
